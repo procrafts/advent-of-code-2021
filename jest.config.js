e3fb3b79dbac36379f34f3b1f446915b4cf77cdf
Franz Fabian Schmid <franzfabian.schmid@brockhaus-ag.de>
2021-12-01 06:22:35 +0100

    initial commit

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.json"
+    }
+  },
+  moduleFileExtensions: [
+    "js",
+    "json",
+    "ts"
+  ],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
+  testMatch: [
+    "**/test/**/*.test.(ts|js)"
+  ],
+  testEnvironment: "node",
+  preset: "ts-jest"
+};
