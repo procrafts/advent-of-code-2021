e802437214fe3ab3216719cbeff30df151f26cba
Franz Fabian Schmid <franzfabian.schmid@brockhaus-ag.de>
2021-12-11 11:05:32 +0100

    fix typo in day-11

--- src/day-11/challenge-1.ts
+++ src/day-11/challenge-1.ts
@@ -3,5 +3,5 @@ import { Octopuses } from './octopuses';
 export default function(): string {
   const octopuses = new Octopuses('input');
   octopuses.doSteps(100);
-  return octopuses.flashes + ' total total flashes after 100 steps';
+  return octopuses.flashes + ' total flashes after 100 steps';
 }
